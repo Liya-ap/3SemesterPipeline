f2c6ee7133a59ec010be86efa8f5474a52f2dc47
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-08 16:18:26 +0200

    added javalin test

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,10 +1,15 @@
 package dat;
 
 import dat.config.HibernateConfig;
+import io.javalin.Javalin;
 import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("pipeline");
+
+        Javalin app = Javalin.create().start(7070);
+
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
